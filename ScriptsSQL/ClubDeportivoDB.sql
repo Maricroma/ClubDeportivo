1e890d1cf6df247eaf7f1b0de04a42acb8ce7b2b
Maricroma <cris.apdg@gmail.com>
2025-06-20 21:09:38 -0300

    Adding mocked data for testing

--- ScriptsSQL/ClubDeportivoDB.sql
+++ ScriptsSQL/ClubDeportivoDB.sql
@@ -1,32 +1,40 @@
-drop database if exists ClubDeportivo;
-create database ClubDeportivo;
-use ClubDeportivo;
+-- Creación de la base de datos 
 
-create table roles(
-RolUsu int,
-NomRol varchar(30),
-constraint primary key(RolUsu)
+DROP DATABASE IF EXISTS ClubDeportivo;
+CREATE DATABASE ClubDeportivo;
+USE ClubDeportivo;
+
+-- Creación e inserción de datos de la tabla roles
+
+CREATE TABLE roles(
+RolUsu INT,
+NomRol VARCHAR(30),
+CONSTRAINT PRIMARY KEY(RolUsu)
 );
 
-insert into roles values
+INSERT INTO roles VALUES
 (120,'Administrador'),
 (121,'Empleado');
 
-create table usuario(
-CodUsu int auto_increment,
-NombreUsu varchar (20),
-PassUsu varchar (15),
-RolUsu int,
-Activo boolean default true,
-constraint pk_usuario primary key (CodUsu),
-constraint fk_usuario foreign key(RolUsu) references roles(RolUsu)
+-- Creación e inserción de datos de la tabla usuario
+
+CREATE TABLE usuario(
+CodUsu INT AUTO_INCREMENT,
+NombreUsu VARCHAR (20) UNIQUE,
+PassUsu VARCHAR (15),
+RolUsu INT,
+Activo BOOLEAN DEFAULT TRUE,
+CONSTRAINT pk_usuario PRIMARY KEY (CodUsu),
+CONSTRAINT fk_usuario FOREIGN KEY(RolUsu) REFERENCES roles(RolUsu)
 );
 
-insert into usuario(CodUsu,NombreUsu,PassUsu,RolUsu) values
-(26,'Mari2023','123456',120);
+INSERT INTO usuario(CodUsu,NombreUsu,PassUsu,RolUsu) values
+('admin','123456',120);
 
-CREATE TABLE Cliente (
-    Id BIGINT PRIMARY KEY,
+-- Creación de la tabla cliente
+
+CREATE TABLE cliente (
+    Id BIGINT PRIMARY KEY AUTO_INCREMENT,
     Nombre VARCHAR(100) NOT NULL,
     Apellido VARCHAR(100) NOT NULL,
     Dni VARCHAR(20) NOT NULL UNIQUE,
@@ -45,8 +53,10 @@ CREATE TABLE Cliente (
     -- ya que se modelan con tablas relacionadas.
 );
 
-CREATE TABLE Cuota (
-    Id BIGINT PRIMARY KEY,
+-- Creación de la tabla cuota
+
+CREATE TABLE cuota (
+    Id BIGINT PRIMARY KEY AUTO_INCREMENT,
     ClienteId BIGINT NOT NULL,
     Fecha DATE NOT NULL,
     Precio DECIMAL(10, 2) NOT NULL,
@@ -56,14 +66,30 @@ CREATE TABLE Cuota (
         ON DELETE CASCADE
 );
 
-CREATE TABLE Actividad (
-    Id BIGINT PRIMARY KEY,
+-- Creación e inserción de datos de la tabla actividad
+
+CREATE TABLE actividad (
+    Id BIGINT PRIMARY KEY AUTO_INCREMENT,
     Nombre VARCHAR(100) NOT NULL,
     Precio DECIMAL(10, 2) NOT NULL
 );
 
-CREATE TABLE PagoActividad (
-    Id BIGINT PRIMARY KEY,
+INSERT INTO actividad(Nombre, Precio) values
+('Fútbol', 3500.00),
+('Básquet', 3200.00),
+('Tenis', 4000.00),
+('Natación', 4500.00),
+('Remo', 4600.00),
+('Gimnasio', 3000.00),
+('Yoga', 2800.00),
+('Pilates', 2900.00),
+('Zumba', 2700.00),
+('Vóley', 3100.00);
+
+-- Creación de la tabla pagoActividad
+
+CREATE TABLE pagoActividad (
+    Id BIGINT PRIMARY KEY AUTO_INCREMENT,
     ClienteId BIGINT NOT NULL,
     Fecha DATE NOT NULL,
     ActividadId BIGINT NOT NULL,
@@ -73,6 +99,76 @@ CREATE TABLE PagoActividad (
     FOREIGN KEY (ActividadId) REFERENCES Actividad(Id)
         ON DELETE CASCADE
 );
+
+--------------------- PROCEDURE LOGIN -------------------------
+/* se cambia el delimitador de linea para poder almacenar en
+el sistema gestor el código del procedimiento
+Se puede utilizar cualquier caracter 
+*************************************************   */
+
+delimiter //  
+create procedure LoginUsuario(in Usu varchar(20),in Pass varchar(15))
+
+/* =============================================================================
+Se colocan dos parametros de entrada:
+uno para el nombre de usuario y el otro para la contraseña
+observar que la longitud debe ser igual que la longitud del atributo de la tabla
+===================================================================================  */
+BEGIN
+  SELECT u.CodUsu, u.NombreUsu, u.PassUsu, r.NomRol
+  FROM usuario u
+  INNER JOIN roles r ON u.RolUsu = r.RolUsu
+  WHERE u.NombreUsu = Usu
+    AND u.PassUsu = Pass
+    AND u.Activo = 1;
+END 
+//
+
+/* ==========================
+si queremos probar el procedure se usa call
+
+call LoginUsuario('dato1', 'dato2')//
+
+si los datos de los parametros existen la consulta arroja 1 FILA
+si los datos de los parametros NO EXISTEN la consulta arroja 0 FILAS
+============================================================================= */
+
+--------------------- PROCEDURE ALTA DE CLIENTE -------------------------
+
+delimiter // 
+
+create procedure AltaCliente(in Nombre varchar(100),in Apellido varchar(100),in Doc varchar(20), in Telefono varchar(20), in Email varchar(50), in FichaMedica tinyint(1), in FechaInscripcion date, in EsSocio tinyint(1), in Carnet tinyint(1), in FechaProximoPago date, out rta int)
+
+begin
+	declare filas int default 0;
+	declare existe int default 0;
+
+	set filas = (select count(*) from cliente);
+	if filas = 0 then
+	set filas = 452; /* consideramos a este numero como el primer numero de cliente */
+	else
+	/* -------------------------------------------------------------------------------
+	buscamos el ultimo numero de cliente almacenado para sumarle una unidad y
+	considerarla como PRIMARY KEY de la tabla
+___________________________________________________________________________ */
+	set filas = (select max(Id) + 1 from cliente);
+	
+	/* ---------------------------------------------------------
+		para saber si ya esta almacenado el cliente
+	------------------------------------------------------- */	
+	set existe = (select count(*) from cliente where Dni = Doc);
+	end if;
+	
+	if existe = 0 then	 
+		insert into cliente values(filas,Nombre,Apellido,Doc,Telefono,Email,FichaMedica,FechaInscripcion,EsSocio,Carnet,FechaProximoPago);
+		set rta  = filas;
+	else
+		set rta = existe;
+	end if;		 
+    
+end //
+
+
 
 
 
